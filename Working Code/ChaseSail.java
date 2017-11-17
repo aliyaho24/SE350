52bec655faba8a08e925952acc56d4fbcce3c235
aliyaho <aliyaho@10.0.0.57>
2017-11-17 14:56:18 -0600

    Observer sail working :)

--- Working Code/ChaseSail.java
+++ Working Code/ChaseSail.java
@@ -1,56 +1,83 @@
-import java.awt.Point;
 
 public class ChaseSail implements SailStrategy {
-		
-	Point pirateLocation, shipLocation;
+
 	OceanMap oceanMap;
 	
 	public void sail(Pirate pirate) {
 		
-		Point pirateLocation = pirate.getPirateLocation();
-		Point shipLocation = pirate.getShipLocation();
-		OceanMap oceanMap = pirate.map;
-
-		if(pirateLocation.x - shipLocation.x < 0) 
-			goEast();
-		else 
-			goWest(); 
+		oceanMap = pirate.map;
+		int shipX = pirate.shipLocation.x;
+		int shipY = pirate.shipLocation.y;
+		int pirateX = pirate.pirateLocation.x;
+		int pirateY = pirate.pirateLocation.y;
 		
-    	 	if(pirateLocation.y - shipLocation.y < 0)
-    	 		goSouth();
-    	 	else
-    	 		goNorth();
-    	 	
-    	 	//update pirate coordinates
-    	 	pirate.setX(pirateLocation.x);
-    	 	pirate.setY(pirateLocation.y);
-	}
-	
-	public void goEast(){
-		if(pirateLocation.x<oceanMap.getDimensions()-1 && 
-			oceanMap.isOcean(pirateLocation.x+1, pirateLocation.y)){
-    				pirateLocation.x++;
-    		} 
-    }
-	
-	public void goWest(){
-	    	if(pirateLocation.x >0 && 
-	    		oceanMap.isOcean(pirateLocation.x-1, pirateLocation.y)){
-	    			pirateLocation.x--;
-	    	}  
-	}
-	    
-	public void goNorth(){
-	    	if(pirateLocation.y>0 && 
-	    		oceanMap.isOcean(pirateLocation.x, pirateLocation.y-1)){
-	    			pirateLocation.y--;
-	    	}  	
-	  }
-	    
-	public void goSouth(){
-	    	if(pirateLocation.y<oceanMap.getDimensions()-1 && 
-	    		oceanMap.isOcean(pirateLocation.x, pirateLocation.y+1)){
-	    			pirateLocation.y++;
-	    		}  
+		if (pirate.shipLocation != pirate.pirateLocation) {
+			
+				//CC is east
+				if(pirateX<oceanMap.getDimensions()-1 && pirateX < shipX && pirateY == shipY &&
+					oceanMap.isOcean(pirateX+1, pirateY)){	
+					pirate.pirateLocation.x++;
+		    		} 
+				
+				//CC is west
+			    	if(pirateX >0 && pirateX > shipX && pirateY == shipY &&
+			    		oceanMap.isOcean(pirateX-1, pirateY)){
+			    			pirate.pirateLocation.x--;
+			    	}  
+			    	
+			    //CC is north
+			    	if(pirateY>0 && pirateY > shipY && pirateX == shipX &&
+			    		oceanMap.isOcean(pirateX, pirateY-1)){
+			    			pirate.pirateLocation.y--;
+			    	}  	
+			    	
+			    //CC is south
+			    	if(pirateY<oceanMap.getDimensions()-1 && pirateY < shipY && pirateX == shipX &&
+			    		oceanMap.isOcean(pirateX, pirateY+1)){
+			    			pirate.pirateLocation.y++;
+			    		}  
+			    	
+			    	//CC is northwest
+			    if (shipX < pirateX && shipY < pirateY) {
+						if (pirateX - 1 > 0 && pirateY - 1 > 0 &&
+							oceanMap.isOcean(pirateX - 1, pirateY - 1)) {
+								pirate.pirateLocation.x--;
+								pirate.pirateLocation.y--;
+						}			
+					}
+				
+			    //CC is northeast
+				if (shipX > pirateX && shipY < pirateY) { 
+						if (pirateX + 1 < oceanMap.getDimensions() &&
+							pirateY - 1 > 0 &&
+							oceanMap.isOcean(pirateX + 1, pirateY - 1)) {
+								pirate.pirateLocation.x++;
+								pirate.pirateLocation.y--;
+						}
+				}
+				
+				//CC is southwest	
+				if (shipX < pirateX && shipY > pirateY) {
+					if (pirateX - 1 > 0 &&
+						pirateY + 1 < oceanMap.getDimensions() &&
+						oceanMap.isOcean(pirateX - 1, pirateY + 1)) {
+							pirate.pirateLocation.x--;
+							pirate.pirateLocation.y++;
+						}
+				}
+				
+				//CC is south east
+				if (shipX > pirateX && shipY > pirateY) {	
+					if (pirateX + 1 < oceanMap.getDimensions() &&
+						pirateY + 1 < oceanMap.getDimensions() &&
+						oceanMap.isOcean(pirateX + 1, pirateY + 1)) {
+							pirate.pirateLocation.x++;
+							pirate.pirateLocation.y++;
+						}
+				} 	
+		}
 	}
 }
+
+
+
